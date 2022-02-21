d83804abcc5bdaaf877ad9509129533e5189f3ae
candacehernandez <chernandez.aggie@gmail.com>
2022-02-21 17:48:49 -0600

    commit tree 289d32698ba0586b5599657199bcf020b5d74373

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+//MOVE DODGER LEFT
+function moveDodgerLeft(){
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if(left > 0){
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+//MOVE DODGER RIGHT
+  function moveDodgerRight(){
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if(left < 360){
+        dodger.style.left = `${left + 1}px`;
+    }
+}
+document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
